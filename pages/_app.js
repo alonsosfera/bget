dfe9a91fa63fa590d44fbf739e65fcf73ba09c32
Alonso Gutierrez <alonso@atriumhq.com>
2022-09-10 01:45:14 -0600

    [Auth] Implements NextAuth authentication with email link

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import { SessionProvider as AuthProvider, useSession } from 'next-auth/react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <AuthProvider session={session}>
+      {Component.auth ? (
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      ) : <Component {...pageProps} />}
+    </AuthProvider>
+  );
+}
+
+function Auth({ children }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true })
+
+  if (status === "loading") {
+    return <div>Loading...</div>
+  }
+  return children
 }
 
 export default MyApp
